790d84cfa9bfe46dc0a20e40d16ff2ba9f7df720
Erwan MISCHKE <erwan.mischke@edu.devinci.fr>
2024-05-14 00:43:22 +0200

    ajout test unitaire

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDijkstra()
+        {
+            // Création des villes pour le test
+            Commande.Ville villeA = new Commande.Ville("Ville A");
+            Commande.Ville villeB = new Commande.Ville("Ville B");
+            Commande.Ville villeC = new Commande.Ville("Ville C");
+            Commande.Ville villeD = new Commande.Ville("Ville D");
+
+            // Définition des distances entre les villes
+            villeA.VoisinsDist.Add(villeB, 10);
+            villeA.VoisinsDist.Add(villeC, 15);
+            villeB.VoisinsDist.Add(villeD, 12);
+            villeC.VoisinsDist.Add(villeD, 5);
+
+            // Création de la liste de villes
+            List<Commande.Ville> villes = new List<Commande.Ville>() { villeA, villeB, villeC, villeD };
+
+            // Appel de la méthode Dijkstra pour calculer les chemins les plus courts
+            Commande commande = new Commande(null, null, null, DateTime.Now, null);
+            commande.Dijkstra(villeA, villes);
+
+            // Vérification des chemins les plus courts calculés pour chaque ville
+            Assert.AreEqual(0, villeA.CheminLePlusCourt); // Ville de départ, donc la distance est de 0
+            Assert.AreEqual(10, villeB.CheminLePlusCourt); // Chemin le plus court entre Ville A et Ville B
+            Assert.AreEqual(15, villeC.CheminLePlusCourt); // Chemin le plus court entre Ville A et Ville C
+            Assert.AreEqual(20, villeD.CheminLePlusCourt); // Chemin le plus court entre Ville A et Ville D
+        }
+    }
+}
---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bada1d6d-541f-4778-856f-75c5868885fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
